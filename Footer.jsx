5923b208eb1d0db088a83fe45f3bd9d66c4cc068
NOEL <gueslynoeljuste87@gmail.com>
2026-07-30 14:32:04 -0400

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer style={{ padding: '1rem 2rem', borderTop: '1px solid #ddd', marginTop: '2rem' }}>
+      <p>© 2026 Portfolio LOG3500</p>
+    </footer>
+  );
+}
+
+export default Footer;
